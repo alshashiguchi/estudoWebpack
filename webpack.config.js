8a04c4fbbd79b758227ddf3ada8ba04a5638392f
André Hashiguchi <alshashiguchi@gmail.com>
2018-06-04 22:22:32 -0300

    Babel-loader, a ponte entre o Webpack e o Babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
   output: { // Saida do processamento
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: { // Cria regras dos loaders que são utilizados para criação
+    rules: [
+      {
+        test: /\.js$/, // Essa chave utiliza expressão regular para saber as extensões que ela ira utilizar
+        exclude: /node_modules/, // Arquivos que serão ignorados
+        use: { // qual loader será utilizado.
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
 };
